bae1d1586d8978c94e177b03f889e7da43b4cd00
Mariano Ryser <rysermariano@gmail.com>
2022-05-30 23:18:22 +0200

    Variable port cambiada

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
@@ -8,6 +9,8 @@ console.log("Peticion recibida ")
 res.send("<h1>App!</h1>")
 } )
 
+const PORT = PROCESS.ENV.PORT
 app.listen(4000, () => {
-    console.log("Servidor escichando en puerto 4000")
+    console.log(`servidor escuchando en puerto ${PORT}`)
+    
 })
