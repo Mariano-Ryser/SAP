194ca304dd77a6e529339f009d708e0015eecaf0
Mariano Ryser <rysermariano@gmail.com>
2022-06-01 22:47:10 +0200

    Conexion Mongo DB , AZURE, HTML, CSS, JS

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res) => {
-console.log("Peticion recibida ")
+mongoose.connect(
+    `mongodb+srv://MR-2291:${process.env.MONGO_DB_PASS}@cluster0.brhpx.mongodb.net/stock-app?retryWrites=true&w=majority`
+    )
+    .then( (result) => {
+        app.listen(PORT, () => {
+            console.log(`servidor escuchando en puerto ${PORT}`)
+        })
+        console.log("Conexion exitosa, mongo db")
+    })
+    .catch((err) => console.log(err))
 
-res.send("<h1>App!</h1>")
-} )
+const productSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    price: Number,
+},
+ { timestamps: true }
+)
+const Product = mongoose.model('Product', productSchema,)
 
-const PORT = process.env.PORT || 4000
-app.listen(PORT, () => {
-    console.log(`servidor escuchando en puerto ${PORT}`)
-    
-})
+ app.use(express.json())
+
+
+ app.post('/api/v1/products', async (req, res, next) => {
+ console.log("Peticion recibida ")
+ console.log({ body: req.body })
+
+ const newProduct = new Product(req.body)
+
+ await newProduct
+ .save()
+ .then( result => {
+     res.status(201).json({ok: true})
+ })
+ .catch((err) => console.log(err))
+
+//  next()
+ } )
+
+app.use(express.static(path.join(__dirname, 'public'))) 
+
+const PORT = process.env.PORT
+
