49bb908c61cce45f12c6c1f40c41e99c89af05d4
Mariano Ryser <rysermariano@gmail.com>
2022-06-20 22:13:00 +0200

    mjsx b98z-('¿+)

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const express = require('express')
+// // // REQUERIMIENTOS Y CONSTANTES
+require('dotenv').config() //Variables de entorno
+const express = require('express') 
 const mongoose = require('mongoose')
-const path = require('path')
-require('dotenv').config()
+var cors = require('cors')  //Cors permiso para interaccion entre servidores
+
+// const path = require('path')  //Para hacer el camino a archivos estaticos
+// const axios = require('axios')  // Para hacer peticiones APIS del lado del servidor
 
 const app = express()
+app.use(cors()) //Ejecutando midelwer
 
+// // // CONECCION CON LA BASE DE DATOS MONGO DB MONGOSE!
 mongoose.connect(
     `mongodb+srv://MR-2291:${process.env.MONGO_DB_PASS}@cluster0.brhpx.mongodb.net/stock-app?retryWrites=true&w=majority`
     )
@@ -14,36 +20,38 @@ mongoose.connect(
         })
         console.log("Conexion exitosa, mongo db")
     })
-    .catch((err) => console.log(err))
+     .catch((err) => console.log(err))
 
-const productSchema = mongoose.Schema({
-    name: {type: String, required: true},
-    price: Number,
-},
- { timestamps: true }
-)
-const Product = mongoose.model('Product', productSchema,)
+
+
+// // CREAMOS EL ESQUEMA DE LA BASE DE DATOS EN MONGO
+    const productSchema = mongoose.Schema({
+            name: {type: String, required: true},
+            price: Number,
+        },
+        { timestamps: true })
+
+    const Product = mongoose.model('Product', productSchema,)  //ALMACENAMOS EN UNA CONSTANTE EL MODELADO DEL ALMACEN
+
 
  app.use(express.json())
 
+ app.post('/api/v1/products',  (req, res) => {
 
- app.post('/api/v1/products', async (req, res, next) => {
- console.log("Peticion recibida ")
- console.log({ body: req.body })
+    const newProduct = new Product(req.body)
 
- const newProduct = new Product(req.body)
-
- await newProduct
- .save()
- .then( result => {
-     res.status(201).json({ok: true})
- })
- .catch((err) => console.log(err))
-
-//  next()
+     newProduct
+         .save()
+         .then( result => {
+         res.status(201).json({ok: true})
+          })
+         .catch((err) => console.log(err))
+        next()
  } )
 
-app.use(express.static(path.join(__dirname, 'public'))) 
+
+ //CON ESTO SERVIMOS LOS ARCHIVOS ESTATICOS DE HMTL CSS Y JS DE LA CARPETA PUBLIC!
+// app.use(express.static(path.join(__dirname, 'public'))) 
 
 const PORT = process.env.PORT
 
